75b721aae2b4b357e1654d79dc1e4d0813df3c99
joshua <jgchoi.qa@gmail.com>
2017-05-26 14:23:51 +0900

    select 쿼리 desc로 변경, fe에 ajax 처리 코드 추가

--- fe/js/app.js
+++ fe/js/app.js
@@ -2,5 +2,30 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
-
+	$( document ).ready(function() {
+		$.ajax({
+			url: './api/todos',
+			method: 'GET',
+			dataType:'json',
+			success: function(data) {
+				var items = [];
+				var itemLeftCount = 0;
+				console.log(data);
+				for(var i = 0; i < data.length; i++) {
+					var className = '';
+					var inputChecked = '';
+					if(data[i].completed == 1) {
+						className = ' class="completed"';
+						inputChecked = ' checked';
+					} else {
+						itemLeftCount++;
+					}
+					items.push("<li" + className + ">" + "<div class='view'><input class='toggle' type='checkbox'" + inputChecked + ">\
+								<label>" + data[i].todo + "</label><button class='destroy'></button></div></li>");
+				}
+				$('.todo-list').html(items);
+				$('.todo-count > strong').text(itemLeftCount);
+			}
+		})	
+	});
 })(window);
--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -3,7 +3,7 @@ package kr.or.connect.todo.persistence;
 public class TodoSqls {
 	static final String COUNT_TODO = "SELECT COUNT(*) FROM todo";
 	static final String SELECT_BY_ID = "SELECT id, todo, completed, date FROM todo where id = :id";
-	static final String SELECT_ALL = "SELECT id, todo, completed, date FROM todo";
+	static final String SELECT_ALL = "SELECT id, todo, completed, date FROM todo ORDER BY id DESC";
 	static final String DELETE_BY_ID = "DELETE FROM todo WHERE id= :id";
 	static final String UPDATE =
 			"UPDATE todo SET\n"
