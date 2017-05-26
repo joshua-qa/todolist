04124ba1411b7d490e458e80d1ccb69e54b8416c
joshua <jgchoi.qa@gmail.com>
2017-05-26 16:50:41 +0900

    UPDATE 쿼리 수정, fe에 completed update ajax 추가

--- fe/js/app.js
+++ fe/js/app.js
@@ -1,12 +1,11 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
-	$( document ).ready(function() {
+	$(document).ready(function() {
 		$.ajax({
 			url: './api/todos',
 			method: 'GET',
-			dataType:'json',
+			dataType: 'json',
 			success: function(data) {
 				var items = [];
 				var itemLeftCount = 0;
@@ -20,12 +19,44 @@
 					} else {
 						itemLeftCount++;
 					}
-					items.push("<li" + className + ">" + "<div class='view'><input class='toggle' type='checkbox'" + inputChecked + ">\
+					items.push("<li" + className + " data-id=" + data[i].id + ">" + "<div class='view'><input class='toggle' type='checkbox'" + inputChecked + ">\
 								<label>" + data[i].todo + "</label><button class='destroy'></button></div></li>");
 				}
 				$('.todo-list').html(items);
 				$('.todo-count > strong').text(itemLeftCount);
 			}
-		})	
+		})
+	});
+
+	
+	$('.todo-list').on('click', '.toggle', function() {
+		var updateId = $(this).parent().parent().data('id');
+		var parent = $(this).parent().parent()
+		console.log("updateId : " + updateId);
+		if(this.checked) {
+			var flag = 1;
+		} else {
+			var flag = 0;
+		}
+
+		$.ajax({
+			url: './api/todos/' + updateId,
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			data: JSON.stringify({completed: flag}),
+			dataType: 'json',
+			success: function(data) {
+				var count = parseInt($('.todo-count > strong').text());
+				if(flag == 1) {
+					$(parent).addClass("completed");
+					$('.todo-count > strong').text(count - 1);
+				} else {
+					$(parent).removeClass("completed");
+					$('.todo-count > strong').text(count + 1);
+				}
+			}
+		})
 	});
 })(window);
--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -5,9 +5,5 @@ public class TodoSqls {
 	static final String SELECT_BY_ID = "SELECT id, todo, completed, date FROM todo where id = :id";
 	static final String SELECT_ALL = "SELECT id, todo, completed, date FROM todo ORDER BY id DESC";
 	static final String DELETE_BY_ID = "DELETE FROM todo WHERE id= :id";
-	static final String UPDATE =
-			"UPDATE todo SET\n"
-			+ "todo = :todo,"
-			+ "completed = :completed\n"
-			+ "WHERE id = :id";
+	static final String UPDATE = "UPDATE todo SET completed = :completed WHERE id = :id";
 }
